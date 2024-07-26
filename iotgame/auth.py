ebd3bf643cf5b47acba95eca19f26d7c0877854f
Donal Heidenblad <dheidenb@umd.edu>
2024-07-26 16:47:22 -0400

    removing auth

--- iotgame/auth.py
+++ iotgame/auth.py
@@ -30,6 +30,7 @@ def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
+            pass # we don't have login yet
             return redirect(url_for('auth.login'))
 
         return view(**kwargs)
